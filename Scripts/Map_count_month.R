dcbfd34bb88207c9ea13b74add97e21df45727a6
JolienGoossens <jolien.goossens@vliz.be>
2018-07-26 15:58:25 +0200

    New values for month

--- Scripts/Map_count_month.R
+++ Scripts/Map_count_month.R
@@ -1,2 +1,20 @@
 #### Load packages ####
 source("Scripts/Organise_countdata.R") # also sources Map_stations.R
+
+#### Data organization
+# rename months
+count$Month <- as.factor(count$Month)
+count$Month <- plyr::revalue(count$Month, c(
+  "1" = "January",
+  "2" = "February",
+  "3" = "March",
+  "4" = "April",
+  "5" = "May",
+  "6" = "June",
+  "7" = "July",
+  "8" = "August",
+  "9" = "September",
+  "10" = "October",
+  "11" = "November",
+  "12" = "December"
+))
