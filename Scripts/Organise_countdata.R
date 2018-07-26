3f72c70e492fd83a6e49e8ed4abf0737fc6851af
JolienGoossens <jolien.goossens@vliz.be>
2018-07-26 14:15:24 +0200

    Change order of station factor levels for plotting

--- Scripts/Organise_countdata.R
+++ Scripts/Organise_countdata.R
@@ -26,4 +26,8 @@ count[count$Station == "lw01",]$Station <- "LW01"
 count[count$Station == "",]$Station <- "ZG02" # "" seems to coincide entirely with ZG02
 count$Station <- as.factor(count$Station)
 
+#### Timeline with data availability ####
+unique(count$Station)
+count$Station <- factor(count$Station, levels = c("LW02", "W10", "LW01", "W09", "W07bis", "435", "421", "W08","780", "330", "ZG02", "710", "230", "215", "700", "130", "120"))
+
 
