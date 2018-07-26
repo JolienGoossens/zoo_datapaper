9597b8139e73fc0e47265c4c348fa225c9f31864
JolienGoossens <jolien.goossens@vliz.be>
2018-07-26 09:48:44 +0200

    Remove code to choose symbol

---
+++ Scripts/Organise_countdata.R
@@ -0,0 +1,29 @@
+#### Load packages ####
+source("Scripts/Map_stations.R")
+library(lubridate)
+library(dplyr)
+
+#### Read data ####
+count <- read.csv2("Data/Count/ZooplanktonCounts.csv", stringsAsFactors = F)
+
+#### Preliminary data treatment ####
+count$X_id <- as.factor(count$X_id)
+count$Taxon <- as.factor(count$Taxon)
+count$Date <- parse_date_time(count$Date, orders = "dmy HM")
+count$Month <- month(count$Date)
+count$Year <- year(count$Date)
+
+# issue with station names
+base_map + 
+  geom_point(data = station, aes(x = long, y = lat, shape = freq), size = 4) +
+  scale_shape_manual(values = c("Monthly" = 18, "Seasonal" = 20), name = "Stations") +
+  geom_text(data = station, aes(x = long, y = lat, label = station), vjust = 0, , nudge_y = 0.02) +
+  geom_point(data = count[count$Station == "",], aes(x = Longitude, y = Latitude), colour = "red")
+
+# rename some station levels 
+count[count$Station == "lw02",]$Station <- "LW02"
+count[count$Station == "lw01",]$Station <- "LW01"
+count[count$Station == "",]$Station <- "ZG02" # "" seems to coincide entirely with ZG02
+count$Station <- as.factor(count$Station)
+
+
